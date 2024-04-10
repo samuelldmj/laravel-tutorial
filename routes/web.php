8a6fd97880648e4c1be795646074b70d6d4f9494
Samuel <samuelldmj5@gmail.com>
2024-04-10 17:29:45 +0100

    Raw sql query in route file

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,23 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    // dd('Samuel');
+    //selecting users
+    $users = DB::select("select * from users");
+
+    //inserting into table in the db
+    // $users = DB::insert("insert into users (name, email, password) values (?, ?, ?)", [ 'Samuel', "samuelldmj@hotmail.com", 4586]);
+    // $users = DB::insert("insert into users (name, email, password) values (?, ?, ?)", [ 'Daniel', "samuelldmj@coldmail.com", 7979]);
+    //  $users = DB::insert("insert into users (name, email, password) values (?, ?, ?)", [ 'Blessing', "blesso@coldmail.com", 2020]);
+
+    //updating data
+    // $users = DB::update("update users set email = blezzo@coldmail.com where id= 3");
+    // $users = DB::update("update users set email = ? where id= ?", ["blezzo@coldmail.com", 3]);
+    dd($users);
 });
 
 Route::get('/dashboard', function () {
