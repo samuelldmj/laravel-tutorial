9d44ce8f834dfe3d0b6d1088ecfacd5a5454f463
Samuel <samuelldmj5@gmail.com>
2024-04-11 16:39:12 +0100

    using eloquent to add, delete and search for data

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,17 @@
 <?php
 
+namespace App\Models;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
 
+
 Route::get('/', function () {
     // return view('welcome');
-    // dd('Samuel');
+   
+
     //selecting users
-    $users = DB::select("select * from users");
+    // $users = DB::select("select * from users");
 
     //inserting into table in the db
     // $users = DB::insert("insert into users (name, email, password) values (?, ?, ?)", [ 'Samuel', "samuelldmj@hotmail.com", 4586]);
@@ -18,7 +21,51 @@ Route::get('/', function () {
     //updating data
     // $users = DB::update("update users set email = blezzo@coldmail.com where id= 3");
     // $users = DB::update("update users set email = ? where id= ?", ["blezzo@coldmail.com", 3]);
+    // dd($users);
+
+    //=========using laravel query builder
+    //getting data
+    // $users = DB::table('users')->get();
+    // $users = DB::table('users')->where('id', [1,2])->get();
+    
+    //inserting data in DB
+    // $users = DB::table('users')->insert([
+    //     'name' => "Dax",
+    //     'email' => 'dax@coldmail.com',
+    //     'password' => 6060
+    // ]);
+
+    //updating data
+    // $users = DB::table('users')->where('id', 1)->update(['email' => 'samuelldmj1@coldmail.com']);
+
+    //=======using eloquent
+    //search for a data
+    // $users = User::where('id', 2)->first();
+
+    //adding a data
+    // $users = User::create([
+    //     'name' => "Mazi",
+    //     'email' => 'mazi@coldmail.com',
+    //     'password' => 6060
+    // ]);
+
+    //updating a data 
+    // $users = User::where('id', 6)->first();
+    // // $users = User::find(6);
+    // $users->update([
+    //     'password' => 3030
+    // ]);
+
+    //deleting user
+    // $users = User::find(6);
+    // $users->delete();
+  
+
+    //getting all data
+    $users = User::get();
     dd($users);
+
+
 });
 
 Route::get('/dashboard', function () {
