986513753eaca6a3e56b30cd3d50eff2c67cd30d
Samuel <samuelldmj5@gmail.com>
2024-04-15 14:00:29 +0100

    edit.blade n web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ namespace App\Models;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Profile\AvatarController;
+
 
 
 Route::get('/', function () {
@@ -76,6 +78,7 @@ Route::get('/dashboard', function () {
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::patch('/profile/avatar', [AvatarController::class, 'update'])->name('profile.avatar');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
