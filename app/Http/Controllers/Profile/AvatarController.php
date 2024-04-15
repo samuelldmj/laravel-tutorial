a8ba2785407543bd268121be04d97465644f7ccc
Samuel <samuelldmj5@gmail.com>
2024-04-15 16:35:37 +0100

    form request

--- app/Http/Controllers/Profile/AvatarController.php
+++ app/Http/Controllers/Profile/AvatarController.php
@@ -2,14 +2,23 @@
 
 namespace App\Http\Controllers\Profile;
 
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\UpdateAvatarRequest;
 
 class AvatarController extends Controller
 {
-  public function update(){
+    //changed the Request class in the update function to the name of the request file i created.
+  public function update(UpdateAvatarRequest $request){
 
-        //back to the profile page.
+    //this should be in the request file
+    // $request->validate([
+    //     'avatar' => 'required|image'
+    // ]);
+    // dd($request->all());
+
+        //back to the profile page after making a change.
     // return response()->redirectTo(route('profile.edit'));
     return back()->with('message', 'Avatar is changed');
   }
